8ab4032a115af18cf209d4ee9f0b74de80ef54df
Britt Young <140761880+britt-young@users.noreply.github.com>
2026-02-06 13:43:02 -0500

    cta responsiveness

--- src/Components/HomeCTA.jsx
+++ src/Components/HomeCTA.jsx
@@ -7,24 +7,40 @@ import RippleProvider from "./Animations/RippleProvider";
 const HomeCTA = () => {
   return (
     <RippleProvider>
-      <div className="pattern-bg text-black px-5 min-h-150 flex items-center">
-        <div data-aos="fade-right" data-aos-duration="800" className="bg-white w-full lg:max-w-7xl shadow-lg shadow-green-900/30 mx-auto flex flex-col lg:flex-row items-center lg:gap-10 gap-5 py-5 px-14 rounded-4xl">
-          <div className="w-1/2 text-left">
-            <h6 className="inline-flex"><SiLeaflet className="size-6 text-green-600 " />call us today</h6>
+      <div className="pattern-bg text-black p-5 min-h-150 flex items-center">
+        <div
+          data-aos="fade-right"
+          data-aos-duration="800"
+          className="bg-white w-full lg:max-w-7xl shadow-lg shadow-green-900/30 mx-auto flex flex-col lg:flex-row items-center justify-around lg:gap-10 gap-5 py-10 lg:py-5 px-14 rounded-4xl"
+        >
+          <div className="lg:w-fit text-left">
+            <h6 className="inline-flex">
+              <SiLeaflet className="size-6 text-green-600 " />
+              call us today
+            </h6>
             <h2>Does Your Yard Need A Refresh?</h2>
             <p>
               Transform your outdoor space with our expert landscaping services.
             </p>
+            <img
+              src={cta}
+              alt="Landscaping graphic"
+              className="lg:hidden flex lg:h-72 pt-10"
+            />
             <button
               data-ripple
-              className="inline-flex rounded-full bg-green-900 border-2 border-green-900 hover:bg-white hover:text-black px-8 py-4 items-center text-white uppercase transition duration-500 gap-2 mt-10 cursor-pointer"
+              className="inline-flex rounded-full bg-green-900 border-2 border-green-900 hover:bg-white hover:text-black px-8 py-4 items-center text-white uppercase transition duration-500 gap-2 mt-0 lg:mt-10 cursor-pointer lg:w-fit w-full justify-center"
             >
               Get a free consult <ArrowRight />
             </button>
           </div>
-          <div className="flex lg:flex-row flex-col  text-green-900 py-10 w-1/2 items-center gap-5">
-            
-            <div className="flex flex-col items-start w-full mt-5 gap-3">
+          <div className="flex lg:flex-row flex-col text-green-900 lg:py-10 lg:w-fit items-start gap-5">
+            <img
+              src={cta}
+              alt="Landscaping graphic"
+              className="hidden lg:flex h-100 z-0"
+            />
+            <div className="lg:flex flex-col items-start w-full gap-3 hidden mt-6 z-10 -ml-20">
               <p className="inline-flex items-center gap-2 font-bold">
                 <CircleCheck /> Garden Beds
               </p>
@@ -46,7 +62,6 @@ const HomeCTA = () => {
                 <CircleCheck /> Paver Install & Repair
               </p>
             </div>
-            <img src={cta} alt="Landscaping graphic" className="h-72" />
           </div>
         </div>
       </div>
